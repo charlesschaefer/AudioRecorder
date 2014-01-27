17a57c48977d1392005c219a97b3839c8e0917d0
Warlley Rezende <warlley@uaiti.com.br>
2014-01-27 12:30:41 -0200

    Added onReady callback function param

--- dist/AudioRecorder.js
+++ dist/AudioRecorder.js
@@ -5,9 +5,10 @@
  * @param string url URL used to send the audio data to the server
  * @param HTMLElement el HTML element where the SWF of Wami will be placed
  * @param string [save_url] Saving URL (if it differs from the URL where the audio data is sent)
+ * @param function [onReadyCb] Callback function fired when SWF is ready
  * @uses Wami http://code.google.com/p/wami-recorder/
  */
-function AudioRecorder(script_base, url, el, save_url) {
+function AudioRecorder(script_base, url, el, save_url, onReadyCb) {
 	this.url = url+"?";
 	if (save_url === undefined) {
 		save_url = url;
@@ -24,7 +25,7 @@ function AudioRecorder(script_base, url, el, save_url) {
 		dataType: 'script',
 		cache: true
 	}).done(function() {
-		Wami.setup({id: 'flash', swfUrl: script_base + "/wami/Wami.swf"});
+		Wami.setup({id: 'flash', swfUrl: script_base + "/wami/Wami.swf", onReady: onReadyCb });
 	});
 }
 
@@ -58,7 +59,7 @@ AudioRecorder.prototype.pause = function() {
  */
 AudioRecorder.prototype.save = function(fn) {
 	$.ajax({
-		url: this.save_url, 
+		url: this.save_url,
 		complete: function(result) {
 			fn && fn(result);
 		}
